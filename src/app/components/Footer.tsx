bf43b2923b3b6d69d1df3952e4d073092a76c992
AbdulRahman Maniar <ar1vuog@gmail.com>
2025-09-19 20:16:45 +0000

    Footer Adjust

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer
-      className="fixed left-0 right-0 bottom-0 h-[6vh] z-[2] flex items-center justify-center pointer-events-none"
+      className="fixed left-0 right-0 bottom-0 h-[6vh] z-[100] flex items-center justify-center pointer-events-none"
     >
-      <div className="w-full h-full bg-transparent backdrop-blur-[200000px] flex items-center justify-center pointer-events-none"></div>
+      <div className="w-full h-full bg-transparent backdrop-blur-[1000px] flex items-center justify-center pointer-events-auto"></div>
     </footer>
   );
 }
